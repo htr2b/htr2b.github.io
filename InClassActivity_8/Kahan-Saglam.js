47788ca0bb5a765fb99faaa6ff1475aafeff3b59
Onur Kahan <128113106+htr2b@users.noreply.github.com>
2025-04-17 15:19:42 +0300

    Add files via upload

---
+++ InClassActivity_8/Kahan-Saglam.js
@@ -0,0 +1,79 @@
+var names = [];
+var scores = [];
+
+var $ = function (id) { return document.getElementById(id); };
+
+
+
+window.onload = function () {
+	$("display_results").onclick = displayResults;
+	$("display_scores").onclick = displayScores;
+	$("add").onclick = addScore;
+
+
+};
+
+function addScore() {
+	var name = $("name").value.trim();
+	var score = parseInt($("score").value);
+
+	if (name === "" || isNaN(score) || score < 0 || score > 100) {
+		alert("You must enter a valid name or score");
+		return;
+	}
+
+	names.push(name);
+	scores.push(score);
+
+	$("name").value = "";
+	$("score").value = "";
+	$("name").focus();
+}
+
+function displayScores() {
+	var table = $("scores_table");
+	table.innerHTML = `
+	<h2>Scores</h2>
+        <tr>
+            <th>Name</th>
+            <th>Score</th>
+        </tr>
+    `;
+
+	for (var i = 0; i < names.length; i++) {
+		table.innerHTML += `
+            <tr>
+                <td>${names[i]}</td>
+                <td>${scores[i]}</td>
+            </tr>
+        `;
+	}
+}
+
+function displayResults() {
+	if (scores.length === 0) {
+		$("results").innerHTML = "<h2>Results</h2><p>No scores available.</p>";
+		return;
+	}
+
+	var total = 0;
+	var highest = scores[0];
+	var temp = 0;
+	for (i = 0; i < scores.length; i++) {
+		total += scores[i];
+		if (scores[i] > highest) {
+			highest = scores[i];
+			temp = i;
+		}
+	}
+
+	var average = total / scores.length;
+
+	$("results").innerHTML = `
+        <h2>Results</h2>
+        <p>Average score = ${average.toFixed(2)}</p>
+        <p>Highest score = ${names[temp]} with a score of ${highest}</p>
+    `;
+}
+
+
