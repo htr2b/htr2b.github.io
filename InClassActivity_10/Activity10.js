44b025070579daa6f550baeb682245f3c05a79d6
Onur Kahan <128113106+htr2b@users.noreply.github.com>
2025-04-25 18:14:39 +0300

    Add files via upload

---
+++ InClassActivity_10/Activity10.js
@@ -0,0 +1,18 @@
+$(function () {
+    $("#birthday").datepicker();
+
+    var languages = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python"
+    ];
+
+    $("#language").autocomplete({
+        source: languages
+    });
+});
